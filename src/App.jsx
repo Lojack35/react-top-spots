9da6a1bf4c4a372514215df95de912a661f4b825
austinloucks <austin.loucks35@gmail.com>
2025-05-16 17:07:40 -0700

    Added page header and top spots data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  // Add your code here
+import "./App.css";
+
+const App = () => {
+  const [topspots, setTopspots] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ccc.helloworldbox.com/items/top_spots")
+      .then((response) => response.data)
+      .then((data) => setTopspots(data));
+  }, []);
 
   return (
-    <>
-      {/* Add your code here */}
+    <div className="container">
+      <h1>San Diego Top Spots</h1>
+      <p>A list of the top 30 places to see in San Diego, California.</p>
+      <pre>{JSON.stringify(topspots, null, 2)}</pre>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
--- src/TopSpots.jsx
+++ src/TopSpots.jsx
@@ -1,11 +1,27 @@
 import TopSpot from "./TopSpot";
 
 const TopSpots = ({ spots }) => {
-    return (
-        <>
-            {/* Add your component here */}
-        </>
-    );
+  return (
+    <div className="container-fluid" data-testid="topspots">
+      {spots.map((topspot) => (
+        <div className="card shadow rounded">
+          <img className="card-img-top" alt="Image Here" />
+          <div className="card-body text-center">
+            <h5 className="card-title">{topspot.name}</h5>
+            <p className="card-text">{topspot.description}</p>
+          </div>
+          <div className="card-footer">
+            <a
+              href={`https://www.google.com/maps?q=${topspot.location[0]},${topspot.location[1]}}`}
+              className="card-link"
+            >
+              View location on Google Maps
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TopSpots;
